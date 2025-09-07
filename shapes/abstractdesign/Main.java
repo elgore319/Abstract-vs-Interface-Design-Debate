509553678e2bdb0f8cda8f2cdec3e1300baa1af0
Eddie Gero <elgore319@gmail.com>
2025-09-07 05:46:26 -0500

    Finally

--- shapes/abstractdesign/Main.java
+++ shapes/abstractdesign/Main.java
@@ -6,16 +6,19 @@ public class Main {
         Shape rectangle = new Rectangle("Blue", "MyRectangle", 4.0, 6.0);
         Shape triangle = new Triangle("Green", "MyTriangle", 3.0, 4.0);
 
-        System.out.println("Circle area: " + circle.getArea());
-        circle.draw();
-        System.out.println();
+    System.out.println(circle.getDescription());
+    System.out.println("Circle area: " + circle.getArea());
+    circle.draw();
+    System.out.println();
 
-        System.out.println("Rectangle area: " + rectangle.getArea());
-        rectangle.draw();
-        System.out.println();
+    System.out.println(rectangle.getDescription());
+    System.out.println("Rectangle area: " + rectangle.getArea());
+    rectangle.draw();
+    System.out.println();
 
-        System.out.println("Triangle area: " + triangle.getArea());
-        triangle.draw();
-        System.out.println();
+    System.out.println(triangle.getDescription());
+    System.out.println("Triangle area: " + triangle.getArea());
+    triangle.draw();
+    System.out.println();
     }
 }
