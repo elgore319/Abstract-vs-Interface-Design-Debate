23bf096f0929e232a53bcd320031a8b8853be3c8
Eddie Gero <elgore319@gmail.com>
2025-09-07 20:34:26 -0500

    Added Menu

--- shapes/abstractdesign/Shape.java
+++ shapes/abstractdesign/Shape.java
@@ -2,15 +2,44 @@ package shapes.abstractdesign;
 
 import shapes.interfaces.Describable;
 
+/**
+ * Abstract base class for geometric shapes.
+ * Stores common properties and defines required methods for all shapes.
+ */
 public abstract class Shape implements Describable {
+    /**
+     * The color of the shape.
+     */
     protected String color;
+    /**
+     * The name of the shape.
+     */
     protected String name;
 
+    /**
+     * Constructs a Shape with the specified color and name.
+     * @param color The color of the shape
+     * @param name The name of the shape
+     */
     public Shape(String color, String name) {
         this.color = color;
         this.name = name;
     }
+
+    /**
+     * Calculates and returns the area of the shape.
+     * @return The area of the shape
+     */
     public abstract double getArea();
+
+    /**
+     * Draws the shape (implementation should print details to console or GUI).
+     */
     public abstract void draw();
+
+    /**
+     * Returns a description of the shape.
+     * @return Description string
+     */
     public abstract String getDescription();
 }
--- shapes/interfacedesign/Calculable.java
+++ shapes/interfacedesign/Calculable.java
@@ -1,5 +1,12 @@
 package shapes.interfacedesign;
 
+/**
+ * Interface for objects that can calculate their area.
+ */
 public interface Calculable {
+    /**
+     * Calculates and returns the area of the object.
+     * @return The area
+     */
     double getArea();
 }
